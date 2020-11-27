b2a3fb4f238fbf5a473229af4e5fed6abc21519c
Iurie Manea <manea.iurie@gmail.com>
2020-11-27 19:07:00 +0200

    First commit

---
+++ BearnSDK.xcframework/ios-arm64/BearnSDK.framework/Headers/BearnSDK.h
@@ -0,0 +1,19 @@
+//
+//  BearnSDK.h
+//  BearnSDK
+//
+//  Created by Iurie Manea on 10/5/20.
+//
+
+#import <Foundation/Foundation.h>
+#import "MyFiziqBearnCommon.h"
+
+//! Project version number for BearnSDK.
+FOUNDATION_EXPORT double BearnSDKVersionNumber;
+
+//! Project version string for BearnSDK.
+FOUNDATION_EXPORT const unsigned char BearnSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BearnSDK/PublicHeader.h>
+
+
