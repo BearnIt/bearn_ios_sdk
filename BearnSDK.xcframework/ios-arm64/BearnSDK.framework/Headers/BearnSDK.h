a4db853fe43264348df7547793d25ade47d1e6aa
Iurie Manea <manea.iurie@gmail.com>
2022-03-01 08:19:16 +0200

    AHI BodyScan

--- BearnSDK.xcframework/ios-arm64/BearnSDK.framework/Headers/BearnSDK.h
+++ BearnSDK.xcframework/ios-arm64/BearnSDK.framework/Headers/BearnSDK.h
@@ -6,7 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
-//#import "MyFiziqBearnCommon.h"
+#import "MyFiziqBearnCommon.h"
 //#import "MyFiziqLoginCommon.h"
 //#import "MyFiziqLogin.h"
 //! Project version number for BearnSDK.
